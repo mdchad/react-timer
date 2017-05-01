8d5a456b73d179960b0a077562bee1dcc0f64cce
Irsyad <irsyad.muhd@gmail.com>
2017-05-01 22:53:56 +0800

    init and refactor

---
+++ src/index.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './index.css';
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            time: '',
+            goal: [],
+            submitted: false
+        }
+        // this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+        // this.handleInput = this.handleInput.bind(this);
+        // this.clear = this.clear.bind(this)
+        // this.start = this.start.bind(this)
+
+        /* a better way to bind this using arrow function as demonstrated below */
+    }
+
+    handleChange = (e) => {
+        var numberChange = parseInt(event.target.value, 0)
+        this.setState({time: numberChange})
+        e.preventDefault();
+    }
+
+    handleSubmit = (e) => {
+        this.setState({submitted: true})
+        e.preventDefault();
+    }
+
+    handleInput = (e) => {
+        this.setState({
+            goal: [e.target.value]
+        })
+    }
+
+    clear = () => {
+        this.setState({
+            time: '',
+            goal: [],
+            submitted: false
+        })
+    }
+
+    start = () => {
+        var duration = this.state.time*1000
+        window.setTimeout(() => {
+            alert('Time is up. Time to set another goal');
+        }, duration);
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Time:
+                        <input
+                            className='textField time'
+                            type="number"
+                            value={this.state.time}
+                            onChange={this.handleChange}
+                            placeholder="time is in seconds"/>
+                    </label>
+                    <br />
+                    <label>
+                        Target:
+                        <input
+                            className='textField'
+                            type="text"
+                            value={this.state.goal}
+                            onChange={this.handleInput} />
+                    </label>
+                    <br />
+                    <button
+                        className="inputSubmit"
+                        type="submit">Submit
+                    </button>
+                </form>
+                {this.state.submitted && <Goals goal={this.state.goal} time={this.state.time} />}
+                {this.state.submitted && <button onClick={this.clear}>Clear</button>}
+                {this.state.submitted && <button onClick={this.start}>Start</button>}
+            </div>
+        )
+    }
+}
+
+const Goals = (props) => {
+    return (
+        <div className="target">
+            {props.goal.map((goal, index) =>
+                <p key={goal.index}>Current target: {goal}</p>
+            )}
+            <p>Duration: {props.time}</p>
+        </div>
+    )
+}
+
+render(<App />, document.getElementById('root'));
