b6e1e0a1372d29ed06b5243a1e5a310cfe9d6bfa
Irsyad <irsyad.muhd@gmail.com>
2017-05-15 22:23:49 +0800

    refactor this.state and fix event

--- src/index.js
+++ src/index.js
@@ -1,102 +1,103 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import './index.css';
 
 
 class App extends Component {
-    constructor(){
-        super();
-        this.state = {
-            time: '',
-            goal: [],
-            submitted: false
-        }
-        // this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
-        // this.handleInput = this.handleInput.bind(this);
-        // this.clear = this.clear.bind(this)
-        // this.start = this.start.bind(this)
-
-        /* a better way to bind this using arrow function as demonstrated below */
+  constructor() {
+    super();
+    this.state = {
+      time: '',
+      goal: [],
+      submitted: false
     }
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    // this.handleInput = this.handleInput.bind(this);
+    // this.clear = this.clear.bind(this)
+    // this.start = this.start.bind(this)
 
-    handleChange = (e) => {
-        let numberChange = parseInt(event.target.value, 0);
-        this.setState({time: numberChange});
-        e.preventDefault();
-    };
+    /* a better way to bind this using arrow function as demonstrated below */
+  }
 
-    handleSubmit = (e) => {
-        this.setState({submitted: true});
-        e.preventDefault();
-    };
+  handleChange = (e) => {
+    let numberChange = parseInt(e.target.value, 0);
+    this.setState({time: numberChange});
+    e.preventDefault();
+  };
 
-    handleInput = (e) => {
-        this.setState({
-            goal: [e.target.value]
-        })
-    };
+  handleSubmit = (e) => {
+    this.setState({submitted: true});
+    e.preventDefault();
+  };
 
-    clear = () => {
-        this.setState({
-            time: '',
-            goal: [],
-            submitted: false
-        })
-    };
+  handleInput = (e) => {
+    this.setState({
+      goal: [e.target.value]
+    })
+  };
 
-    start = () => {
-        let duration = this.state.time*1000;
-        window.setTimeout(() => {
-            alert('Time is up. Time to set another goal');
-        }, duration);
-    };
+  clear = () => {
+    this.setState({
+      time: '',
+      goal: [],
+      submitted: false
+    })
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Time:
-                        <input
-                            className='textField time'
-                            type="number"
-                            value={this.state.time}
-                            onChange={this.handleChange}
-                            placeholder="time is in seconds"/>
-                    </label>
-                    <br />
-                    <label>
-                        Target:
-                        <input
-                            className='textField'
-                            type="text"
-                            value={this.state.goal}
-                            onChange={this.handleInput} />
-                    </label>
-                    <br />
-                    <button
-                        className="inputSubmit"
-                        type="submit">Submit
-                    </button>
-                </form>
-                {this.state.submitted && <Goals goal={this.state.goal} time={this.state.time} />}
-                {this.state.submitted && <button onClick={this.clear}>Clear</button>}
-                {this.state.submitted && <button onClick={this.start}>Start</button>}
-            </div>
-        )
-    }
+  start = () => {
+    let duration = this.state.time * 1000;
+    window.setTimeout(() => {
+      alert('Time is up. Time to set another goal');
+    }, duration);
+  };
+
+  render() {
+    const { time, goal, submitted } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Time:
+            <input
+              className='textField time'
+              type="number"
+              value={ time }
+              onChange={ this.handleChange }
+              placeholder="time is in seconds"/>
+          </label>
+          <br />
+          <label>
+            Target:
+            <input
+              className='textField'
+              type="text"
+              value={ goal }
+              onChange={ this.handleInput }/>
+          </label>
+          <br />
+          <button
+            className="inputSubmit"
+            type="submit">Submit
+          </button>
+        </form>
+        {submitted && <Goals { ...this.state } />}
+        {submitted && <button onClick={ this.clear }>Clear</button>}
+        {submitted && <button onClick={ this.start }>Start</button>}
+      </div>
+    )
+  }
 }
 
 const Goals = ({time, goal}) => {
-    return (
-        <div className="target">
-            {goal.map(({goal: {index}}) =>
-                <p key={index}>Current target: {goal}</p>
-            )}
-            <p>Duration: {time}</p>
-        </div>
-    )
+  return (
+    <div className="target">
+      { goal.map(goal =>
+        <p key={ goal.index }>Current target: { goal }</p>
+      )}
+      <p>Duration: { time }</p>
+    </div>
+  )
 };
 
 render(<App />, document.getElementById('root'));
